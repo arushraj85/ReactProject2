15ab5e68cf3a5a1e1143e0287e69e9204f2d31b8
arushraj85 <arushraj85@gmail.com>
2023-10-06 14:14:36 +0530

    passing function from child to parent

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,28 +1,15 @@
-import UserList from './components/userList/userList';
-import './App.css';
-import Login from './components/Login/Login';
-import {useState} from 'react';
-
-
+import UserList from "./components/userList/userList";
+import "./App.css";
+import Login from "./components/Login/Login";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, changeIsLoggedIn] = useState(false);
+  function onLogin() {
+    changeIsLoggedIn(true);
+  }
 
-  const[isLoggedIn,changeIsLoggedIn]=useState(false);
-  function changeHandler(){
-    if(isLoggedIn){
-      changeIsLoggedIn(false) 
-    }
-    else{
-      changeIsLoggedIn(true)
-    }
-    }
-
-  return <div>
-  <button onClick={changeHandler}>Change</button>
-   {
-    (isLoggedIn)? <UserList/> :<Login/>
-   }
-   </div>
+  return <div>{isLoggedIn ? <UserList /> : <Login onLogin={onLogin} />}</div>;
 }
 
 export default App;
--- myapp/src/components/Login/Login.js
+++ myapp/src/components/Login/Login.js
@@ -1,26 +1,33 @@
-import './Login.css';
+import "./Login.css";
 
-function Login(){
-    return <div className="loginPage">
-        <h1>Login Page</h1>
-        <div className="loginFormBox">
-            <form className="loginForm">
-            <div>
+function Login(props) {
+  function onClickHandler(e) {
+    console.log("onClickhanlder clicked");
+    e.preventDefault();
+    props.onLogin();
+  }
+  return (
+    <div className="loginPage">
+      <h1>Login Page</h1>
+      <div className="loginFormBox">
+        <form className="loginForm">
+          <div>
             <label id="username">Username</label>
             <input type="text" placeholder="username"></input>
-        </div>
-        <div>
+          </div>
+          <div>
             <label id="password">Password</label>
             <input type="password" placeholder="password"></input>
-        </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
-
-            </form>
-        </div>
-        
+          </div>
+          <div>
+            <button onClick={onClickHandler} type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+  );
 }
 
 export default Login;
